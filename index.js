ee6f696710e00d4e35a212b319a0397cbc5e9f3e
Mike <mike.munene@student.moringaschool.com>
2023-03-25 15:28:26 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solution here
+
+function findMatching(arr, name) {
+
+    const newArr = [];
+
+    arr.filter((driverName) => { driverName === name || driverName == name.toLowerCase() ? newArr.push(driverName) : newArr });
+
+    return newArr;
+}
+
+function fuzzyMatch(arr, name) {
+    const newArr = [];
+
+    arr.filter((driverName) => { driverName.charAt(0) === name.charAt(0) ? newArr.push(driverName) : newArr });
+
+    return newArr;
+}
+
+function matchName(arr, name) {
+    const newArr = [];
+
+    arr.filter((obj) => obj.name === name ? newArr.push(obj) : newArr);
+
+    return newArr;
+}
